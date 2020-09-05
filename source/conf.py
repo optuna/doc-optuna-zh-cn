af9149b7b81050c5a429dd0c7d898ed82f1990a2
chenghuzi <tigeraus@icloud.com>
2020-09-05 14:56:29 -0400

    update to 1.5

--- source/conf.py
+++ source/conf.py
@@ -70,7 +70,7 @@ master_doc = 'index'
 #
 # This is also used if you do content translation via gettext catalogs.
 # Usually you set "language" from the command line for these cases.
-language = 'zh_CN'
+language = None
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
@@ -163,6 +163,6 @@ texinfo_documents = [
 intersphinx_mapping = {'python': ('https://docs.python.org/3', None)}
 
 # -- Extension configuration -------------------------------------------------
-
-locale_dirs = ['locale/']   # path is example but recommended.
+# path is example but recommended.
+locale_dirs = ['locale/']
 gettext_compact = False     # optional.
